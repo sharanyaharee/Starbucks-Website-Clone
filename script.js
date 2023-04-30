30c0d7ddcef8164fbe8bbdb54a08382da669587e
Sharanya <sharu.sesh@gmail.com>
2023-04-30 16:25:53 +0400

    Dark/light Mode added

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let themeSwitch = document.getElementById("themeSwitch");
+let body = document.querySelector("body");
+themeSwitch.onclick = function () {
+  themeSwitch.classList.toggle("bi-moon-fill");
+
+  body.classList.toggle("dark");
+};
+
