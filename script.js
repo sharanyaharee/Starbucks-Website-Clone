3a76240566e304748582978c710effde77768844
Sharanya <sharu.sesh@gmail.com>
2023-04-30 16:35:56 +0400

    final commit

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let themeSwitch = document.getElementById("themeSwitch");
 let body = document.querySelector("body");
 themeSwitch.onclick = function () {
   themeSwitch.classList.toggle("bi-moon-fill");
-
   body.classList.toggle("dark");
 };
 
